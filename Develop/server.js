cb10202036624dc8a2a26d3c72469ae9a3d1293f
Tifni Shroll <tasshroll@gmail.com>
2023-04-29 15:31:22 -0700

    Create a delete path and pull id of note

--- Develop/server.js
+++ Develop/server.js
@@ -34,10 +34,8 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
 });
 
-
 //`POST /api/notes` should receive a new note to save on the request body, add it to the `db.json` file, 
-//and then return the new note to the client. You'll need to find a way to give each note a unique id when 
-//it's saved (look into npm packages that could do this for you).
+//and then return the new note to the client. Each note is given a unique id using nanoid package
 app.post('/api/notes', (req, res) => {
     // Log that post request was received
     console.log(`${req.method} request was received to add a new note`);
@@ -56,6 +54,29 @@ app.post('/api/notes', (req, res) => {
     };
 });
 
+// fetch(`/api/notes/${id}`
+// get the query param 
+app.delete('/api/notes/:id', (req, res) => {
+    console.log(`${req.method} request was received to delete a new note`);
+    // Get the id out of the URL (?id=a234Jsldkfjs2345234)
+    // console.log ("Req is ", req.query);
+    const id = req.params.id;
+    console.log("Query is : ", id);
+
+}); // end app.delete
+
+
+// app.delete('/api/notes/', (req, res) => {
+//     // Log that post request was received
+//     console.log(`${req.method} request was received to add a new note`);
+
+// const deleteNote = (id) =>
+//   fetch(`/api/notes/${id}`, {
+//     method: 'DELETE',
+//     headers: {
+//       'Content-Type': 'application/json',
+//     },
+//   });
 app.listen(PORT, () =>
     console.log(`Express server listening on port ${PORT}`)
 );
@@ -71,16 +92,14 @@ function addNote(newNote, res) {
         }
         const notesArr = JSON.parse(storedNotes);
         notesArr.push(newNote);
-        //const jsonNotesArr = JSON.stringify(notesArr);
+        console.log("notesArr is ", notesArr);
 
+        // null is used to replace any function values in the object with null to ensure that 
+        // the stringified object is valid JSON. The argument 4 specifies the number of spaces to 
+        // use for indentation, by adding 4 spaces of indentation for each nested level. 
+        const jsonNotesArr = JSON.stringify(notesArr, null, 4); // pretty print with 4 spaces indentation
 
-
-        const formattedNotesArr = notesArr.map(({title, text}) => ({title, text}));
-        const jsonNotesArr = JSON.stringify(formattedNotesArr, null, 4); // pretty print with 4 spaces indentation
-
-
-
-        console.log("DB is ", jsonNotesArr);
+        console.log("Stringified DB is ", jsonNotesArr);
 
         fs.writeFile('./db/db.json', jsonNotesArr, (err) => {
             if (err) {
@@ -92,4 +111,46 @@ function addNote(newNote, res) {
             }
         }); // end of writeFile
     }); // end of readFile
-}
+} // end of add Note
+
+
+function removeNote(id, res) {
+    // Add new note to db.json
+    // Read contents of db.json, parse the ddata, and then push new note object into the array
+    fs.readFile('./db/db.json', (err, storedNotes) => {
+        if (err) {
+            console.log(err);
+            return res.status(500).send("Error reading db");
+        }
+        const notesArr = JSON.parse(storedNotes);
+        for (let i = 0; i < notesArr.length; i++) {
+            const noteID = notesArr[i].id;
+            if (noteID = id) {
+                console.log("Object to remove is :", notesArr[i]);
+                // remove that object
+                notesArr.splice(i, 1); // remove 1 object at index i
+            }
+        }
+        console.log("notesArr with the deleted note is ", notesArr);
+
+        // null is used to replace any function values in the object with null to ensure that 
+        // the stringified object is valid JSON. The argument 4 specifies the number of spaces to 
+        // use for indentation, by adding 4 spaces of indentation for each nested level. 
+        const jsonNotesArr = JSON.stringify(notesArr, null, 4); // pretty print with 4 spaces indentation
+
+        console.log("Stringified DB is ", jsonNotesArr);
+
+        fs.writeFile('./db/db.json', jsonNotesArr, (err) => {
+            if (err) {
+                console.log(err)
+            } else {
+                console.log('Note deleted from the database was', newNote);
+                // Send client the newNote in the response
+                res.json(notesArr);
+            }
+        }); // end of writeFile
+    }); // end of readFile
+} // end of add Note
+
+
+
